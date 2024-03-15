ba1e9452993e18f1b179b2b23baf68468cce2ae3
Charles Ofuasia <ofu23001@byui.edu>
2024-03-15 11:00:45 +0100

    Revert "completed week 3 assignment"
    This reverts commit feeec4c1f3dec83a0ce421f890c7dd9a70f543b5.

--- server.js
+++ server.js
@@ -45,8 +45,7 @@ app.use(async (err, req, res, next) => {
   if (err.status == 404) {
     message = err.message;
   } else {
-    message =
-      "Oops! Sorry, the link appears not to work. Click the Home to return.";
+    message = "Oh no! There was a crash. Maybe try a different route?";
   }
   res.render("errors/error", {
     title: err.status || "Server Error",
